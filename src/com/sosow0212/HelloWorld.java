a0e63dd4aea26cd7899e5e058752693413aa36bd
sosow0212 <sosow0212@naver.com>
2021-10-17 01:00:33 +0900

    Hello world

---
+++ src/com/sosow0212/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.sosow0212;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/sosow0212/Main.java
@@ -0,0 +1,8 @@
+package com.sosow0212;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
