33404c8eacb060b4f83051dad493b47617519788
sosow0212 <sosow0212@naver.com>
2021-10-22 17:30:31 +0900

    .

---
+++ src/com/sosow0212/이론/유용한내장함수.java
@@ -0,0 +1,16 @@
+package com.sosow0212.이론;
+
+import java.util.*;
+
+public class 유용한내장함수 {
+    public static void main(String[] args) {
+        String text = "20";
+        int number = 10;
+
+        // Integer.parseInt(text) => 문자를 정수형으로 변환
+
+        String a = "Java";
+        String b = "Java";
+        System.out.println(a.equals(b));
+    }
+}
