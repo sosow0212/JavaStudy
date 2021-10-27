73fb35e7d2c31f81aa2759604b422f9ad07eee2f
sosow0212 <sosow0212@naver.com>
2021-10-27 14:28:24 +0900

    .

--- src/com/sosow0212/이론/유용한내장함수.java
+++ src/com/sosow0212/이론/유용한내장함수.java
@@ -16,5 +16,9 @@ public class 유용한내장함수 {
         // .length => 배열을 반환
         // .length() => 문자열을 반환
         // size() => 컬렉션 프레임워크 반환(ArrayList, ...)
+
+        // .substring(시작지점, 끝지점) => 문자열의 특정 부분을 뽑는다.
+        // .toUpperCase() => 대문자 변환
+        // .toLowerCase() => 소문자 변환
     }
 }
